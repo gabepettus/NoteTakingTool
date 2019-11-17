b9fa9b4bd23483f41cd32fb8cfe5a9c0181fcdc2
gdpettus <gabe.pettus@gmail.com>
2019-11-17 13:40:07 -0700

    initial files

---
+++ public/assets/js/server.js
@@ -0,0 +1,75 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const manageReservations = require('./data/manageReservations');
+const getReservations = manageReservations[0];
+const storeReservations = manageReservations[1];
+
+const app = express();
+const PORT = 3005;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// tell Express to serve static files in public and data directories
+app.use(express.static("public"));
+app.use(express.static("data"));
+
+// Start the server & listen to incoming requests
+app.listen(PORT, function() {
+    console.log("We're running on port: " + PORT);
+    console.log('server is running and listening on http://localhost:' + PORT)
+});
+
+// Routes
+    // html pages
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(`${__dirname}/public`, "index.html"));
+        
+    });
+    
+    app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(`${__dirname}/public`, "reserve.html"));
+    });
+
+    app.get("/view", function(req, res) {
+    res.sendFile(path.join(`${__dirname}/public`, "view.html"));      
+        
+    });
+
+    // routes to return json
+    // reservations
+    app.get("/api-reserve", function(req, res) {
+        const results = getReservations();
+        res.json(results);
+    });
+
+    // tables
+    app.get("/api-tables", function(req, res) {
+    res.json(tables);
+    });
+
+// array to hold reservations
+var reservations = [
+    {
+    customerName: "yoda",
+    customerPhone: "1-800-Yoda",
+    customerEmail: "Jedi Master",
+    customerID: 666,
+    }
+]
+
+// post new reservation
+app.post("/api-reserve", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    
+    let newReservation = req.body;
+    
+    console.log(newReservation);
+    
+    //add the json the user sent to the character array
+    // reservations.push(newReservation);
+    
+    //display the JSON to the users
+    res.json(newReservation);
+    storeReservations(newReservation);
+    });
